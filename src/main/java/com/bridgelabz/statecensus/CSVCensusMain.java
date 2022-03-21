2963e847ba81185fc537bbb667a2c109d6319bba
SK <senthilptj@gmail.com>
2022-03-21 22:51:45 +0530

    State Census Analyser - CSV

---
+++ src/main/java/com/bridgelabz/statecensus/CSVCensusMain.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.statecensus;
+
+public class CSVCensusMain {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Census Analyser Problem");
+    }
+}
